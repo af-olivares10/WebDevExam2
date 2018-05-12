1afa37414c041866a3ae0eddb92d23c7502f8706
Andrés <af.olivares10@uniandes.edu.co>
2018-05-12 01:38:07 -0500

    Finished data search

--- exam2/imports/ui/App.js
+++ exam2/imports/ui/App.js
@@ -1,95 +1,223 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {withTracker} from "meteor/react-meteor-data"
+import { DatePicker, Input,Form,  Button,Icon , Select} from 'antd';
+const { TextArea } = Input;
+const FormItem = Form.Item;
+import Graph from "./Graph";
+import 'antd/dist/antd.css';
 import * as d3 from "d3"
-export default  class App extends Component{
+export  class App extends Component{
   constructor(props){
     super(props);
-
     this.state={
-      json:{},
-      route:0
+      data:[],
+      agencies:[],
+      agency:0,
+      routes:[],
+      route:0,
+      sClasses:[],
+      sClass:0,
+      directions:[],
+      direction:0,
+      sClassesObj:[],
+      selected:0
     };
   }
 
   componentDidMount() {
-    fetch('https://gist.githubusercontent.com/john-guerra/6a1716d792a20b029392501a5448479b/raw/e0cf741c90a756adeec848f245ec539e0d0cd629/sfNSchedule')
+    this.getAgencies();
+  }
+  getAgencies =()=>{
+    fetch('http://webservices.nextbus.com/service/publicJSONFeed?command=agencyList')
+    .then((response) => response.json())
+    .then((agencies) => {
+      let agenciesTemp = [];
+      for(let agency of agencies["agency"] ){
+        agenciesTemp.push(agency.tag)
+      }
+      this.setState({agencies:agenciesTemp});
+    }
+  )
+  .catch((error) => {
+    console.error(error);
+  });
+}
+
+
+handleSubmit = (e) => {
+  e.preventDefault();
+  this.props.form.validateFields((err, values) => {
+    if (!err) {
+      console.err(values);
+    }
+  });
+}
+handleAgChange=(agency) =>{
+  this.setState({routes:[],sClasses:[],directions:[]})
+  fetch('http://webservices.nextbus.com/service/publicJSONFeed?command=routeList&a='+agency)
+  .then((response) => response.json())
+  .then((responseJson) => {
+
+    if(responseJson["route"]){
+      let x = responseJson["route"].tag?[]:responseJson["route"];
+      if(x.length===0)x.push(responseJson["route"]);
+      this.setState({routes:x,agency:agency});
+    }
+    else{
+      this.setState({routes:[{tag:"No data provided"}],agency:agency});
+
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+handleRouteChange = (route)=>{
+  if(route!=="No data provided"){
+    this.setState({sClasses:[],directions:[]})
+    fetch('http://webservices.nextbus.com/service/publicJSONFeed?command=schedule&a='+this.state.agency+'&r='+route)
     .then((response) => response.json())
     .then((responseJson) => {
-      console.log(responseJson);
-      this.setState({json:responseJson});
-      let selectedRoute = responseJson.route[this.state.route]
-      let buses = []
-      for (let bus of selectedRoute.tr) {
-        let route = bus.stop.filter((d) => d.content!=="--");
-        route.forEach((d) => d.date = new Date(+d.epochTime));
-        buses.push(route);
+      if(responseJson["route"]){
+        let x = responseJson["route"].serviceClass?[]:responseJson["route"];
+        if(x.length===0)x.push(responseJson["route"]);
+        let sClasses =[];
+        for(let r of x){
+          if(sClasses.indexOf(r.serviceClass)===-1){
+            sClasses.push(r.serviceClass);
+          }
+        }
+        this.setState({sClasses:sClasses,route:route,data:x});
       }
-
-
-      const height = 600;
-      const width = 1000;
-
-      const svg = d3.select("#here")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-
-      const margin = ({top: 20, right: 30, bottom: 30, left: 150});
-      const minDate = d3.min(buses[1], d => d.date);
-      const maxDate = new Date(minDate.getTime() + 22*60*60*1000); // minDate + 24 hours
-      const x = d3.scaleTime()
-      .domain([ minDate, maxDate ])
-      .range([margin.left, width - margin.right]);
-      const y = d3.scaleBand()
-      .domain(d3.range(buses[1].length))
-      .rangeRound([height - margin.bottom, margin.top]);
-
-      const xAxis = g => g
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x))
-      // .call(g => g.select(".domain").remove());
-      const yAxis = g => g
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y)
-      .tickFormat((d) => selectedRoute.header.stop[d].content));
-
-      const line = d3.line()
-      .x(d => x(d.date))
-      .y((d,i) => y(i) + y.bandwidth()/2);
-
-      svg.append("g")
-      .call(xAxis);
-
-      svg.append("g")
-      .call(yAxis);
-
-      svg.selectAll(".routes")
-      .data(buses)
-      .enter()
-      .append("path")
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 2)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("d", line);
-      return svg.node();
-
+      else{
+        this.setState({sClasses:["No data provided"],route:route});
+        //  this.setState({routes:[{tag:"No data provided"}],agency:agency});
+      }
     })
     .catch((error) => {
+      console.log(error);
     });
   }
-  joinView= (b)=>{
-    this.setState({route: b});
-  }
-  render(){
-    return(
-    <div >
-      <div id = "here"></div>
-      <input type = "text" className = "input" placeholder="Nickname" ref = "text"/>
-        <button type="button" className="btn howTo-button" onClick={()=>{this.joinView(this.refs.text)}} >Enter</button>
-    </div>
-    )
-  }
 }
+handleSClassChange = (sClass)=>{
+  if(sClass!=="No data provided"){
+  this.setState({directions:[]})
+  let sClasses = this.state.data.filter((d) => d.serviceClass===sClass);
+  let directions =[];
+  for(let r of sClasses){
+    if(directions.indexOf(r.direction)===-1){
+      directions.push(r.direction);
+    }
+  }
+  this.setState({directions:directions,sClass:sClass,sClassesObj:sClasses});
+}
+
+}
+handleDirectionChange = (direction)=>{
+
+  let route = this.state.sClassesObj.filter((d) => d.direction===direction)[0];
+  this.setState({selected:route});
+}
+render(){
+  const { getFieldDecorator } = this.props.form;
+
+  return(
+    <div >
+      <div className = "container" style = {{marginTop:"70px",marginBottom:"70px"}} className = "form">
+
+        <Form onSubmit={this.handleSubmit} >
+          <div className = "form-text">Agency name</div>
+          <FormItem className = "form-item">
+            {getFieldDecorator("agency", {
+              rules: [{ required: true }],
+            })(
+              <Select placeholder="Agency" onChange={this.handleAgChange} >
+                {
+                  this.state.agencies.map(
+                    (ag) => (<Select.Option value={ag} key={ag}>{ag}</Select.Option>)
+                  )
+                }
+              </Select>
+            )}
+          </FormItem>
+          <FormItem className = "form-item">
+            <div className = "form-text">Route</div>
+
+            { (this.state.routes.length>0)?
+              (getFieldDecorator("route", {
+                rules: [{ required: true}],
+              })(
+                <Select placeholder="Route"  onChange={this.handleRouteChange}>
+                  {
+                    this.state.routes.map(
+                      (r) =>
+                      (<Select.Option value={r.tag} key={r.tag}>{r.tag}</Select.Option>)
+                    )
+                  }
+                </Select>
+              )):
+              (<Select defaultValue="lucy"  disabled>
+                <Option value="lucy">Route</Option>
+              </Select>)
+
+            }
+          </FormItem>
+          <FormItem className = "form-item">
+            <div className = "form-text">Service class</div>
+            { (this.state.sClasses.length>0)?
+              (getFieldDecorator("sClass", {
+                rules: [{ required: true}],
+              })(
+                <Select placeholder="Service class"  onChange={this.handleSClassChange}>
+                  {
+                    this.state.sClasses.map(
+                      (r) =>
+                      (<Select.Option value={r} key={r}>{r}</Select.Option>)
+                    )
+                  }
+                </Select>
+              )):
+              (<Select defaultValue="lucy"  disabled>
+                <Option value="lucy">Service class</Option>
+              </Select>)
+
+            }
+          </FormItem>
+          <FormItem className = "form-item">
+            <div className = "form-text">Direction</div>
+            { (this.state.directions.length>0)?
+              (getFieldDecorator("direction", {
+                rules: [{ required: true}],
+              })(
+                <Select placeholder="Service class"  onChange={this.handleDirectionChange}>
+                  {
+                    this.state.directions.map(
+                      (r) =>
+                      (<Select.Option value={r} key={r}>{r}</Select.Option>)
+                    )
+                  }
+                </Select>
+              )):
+              (<Select defaultValue="lucy"  disabled>
+                <Option value="lucy">Service class</Option>
+              </Select>)
+
+            }
+          </FormItem>
+          {/* <FormItem style={{  marginBottom: "5px" }}>
+            <div style ={{  color: "red" }}></div>
+            <Button type="primary" htmlType="submit"><Icon type="right-circle-o" />GO!</Button>
+            <br></br>
+          </FormItem> */}
+        </Form>
+
+      </div>
+      <Graph selected= {this.state.selected}></Graph>
+    </div>
+  )
+}
+}
+const WrappedApp = Form.create()(App);
+
+export default WrappedApp;
